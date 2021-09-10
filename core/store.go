02bbd23483a0133af44ce93f3eacb21547cad9b5
Muhammad Bin Jamil <binjamil35@gmail.com>
2021-09-11 03:52:30 +0500

    Add core key-value functionality

---
+++ core/store.go
@@ -0,0 +1,29 @@
+package core
+
+import "errors"
+
+var store = make(map[string]string)
+
+var ErrorNoSuchKey = errors.New("no such key")
+
+func Get(key string) (string, error) {
+	value, ok := store[key]
+
+	if !ok {
+		return "", ErrorNoSuchKey
+	}
+
+	return value, nil
+}
+
+func Put(key string, value string) error {
+	store[key] = value
+
+	return nil
+}
+
+func Delete(key string) error {
+	delete(store, key)
+
+	return nil
+}
