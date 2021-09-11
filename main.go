c07e3a8a97d3db94fa8a1d002ea4b78839e51a0c
Muhammad Bin Jamil <binjamil35@gmail.com>
2021-09-11 22:43:11 +0500

    Add TLS feature

--- main.go
+++ main.go
@@ -3,12 +3,16 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/binjamil/keyd/service"
 	"github.com/gorilla/mux"
 )
 
 func main() {
+	tlsCert := os.Getenv("TLS_CERTIFICATE")
+	tlsKey := os.Getenv("TLS_KEY")
+
 	err := service.InitializeTransactionLog()
 	if err != nil {
 		panic(err)
@@ -20,5 +24,11 @@ func main() {
 	r.HandleFunc("/v1/{key}", service.PutHandler).Methods(http.MethodPut)
 	r.HandleFunc("/v1/{key}", service.DeleteHandler).Methods(http.MethodDelete)
 
-	log.Fatal(http.ListenAndServe(":8080", r))
+	if tlsCert != "" && tlsKey != "" {
+		log.Println("TLS configuration found. Server started over https...")
+		log.Fatal(http.ListenAndServeTLS(":8080", tlsCert, tlsKey, r))
+	} else {
+		log.Println("TLS configuration not found. Server started over http...")
+		log.Fatal(http.ListenAndServe(":8080", r))
+	}
 }
